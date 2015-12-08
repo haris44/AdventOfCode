8631e2933bc2fc2ebdf919d0a4455646c0aeb958
Alexandre BERTRAND <lxndr.bertrand@gmail.com>
2015-12-08 21:38:15 +0100

    Day C2

---
+++ Day 7 - C 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7___C_2
+{
+    class Program
+    {
+
+        public static Dictionary<string, int> openWith;
+        static void Main(string[] args)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Alexandre\Documents\AdventOfCode\Day 7 - C 2\day7.txt");
+            openWith = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                string[] part = line.Split(' ');
+                string key = part[part.Length - 1];
+                string arg;
+
+                if(part.Length == 3)
+                {
+                    arg = "ADD"
+                }
+                else if(part.Length == 4)
+                {
+                    arg = "NOT";
+                }
+                else if(part.Length == )
+
+
+            }
+        }
+    }
+}
--- Day7 C/Program.cs
+++ Day7 C/Program.cs
@@ -8,134 +8,113 @@ namespace Day7_C
 {
     class Program
     {
-        public static Dictionary<string, ushort> openWith;
+        public static Dictionary<string, int> openWith;
         static void Main(string[] args)
         {
 
-            string[] lines = System.IO.File.ReadAllLines(@"c:\users\macpro\documents\visual studio 2015\Projects\Day7 C\Day7 C\day7.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Alexandre\Documents\AdventOfCode\Day7 C\day7.txt");
 
-            openWith = new Dictionary<string, ushort>();
+            openWith = new Dictionary<string, int>();
 
             foreach (var line in lines)
             {
                 string[] mots = line.Split(' ');
-                ushort int16NotB = 0;
 
                 if (mots[0] == "NOT")
-                {
-                    Manipulate(mots[1]);
-                    Manipulate(mots[3]);
-                    ushort b = (byte)openWith[mots[1]];
-
-                    if (mots[3] == "a" || mots[3] == "lx" || mots[3] == "lw")
-                    {
-                        bool lol;
-                    }
-
-                    int16NotB = (ushort)~b;
-
-                    openWith[mots[3]] = int16NotB;
+                {                  
+                    ManipulateFin(mots[3]);
+                    openWith[mots[3]] = (ushort)Manipulate(mots[1]);
                 }
-                else if (mots.Length == 3)
+
+                else if (mots.Length == 3 && mots[1] == "->")
                 {
-                    try {
-                        Manipulate(mots[2]);
-                        openWith[mots[2]] = ushort.Parse(mots[0]);
-                    }
-                    catch
-                    {
-                        Manipulate(mots[2]);
+                    int m1 = 0;
+                    int.TryParse(mots[0], out m1);
 
-                        if (mots[2] == "a" || mots[2] == "lx" || mots[2] == "lw")
-                        {
-                            bool lol;
-                        }
-
-                        Manipulate(mots[0]);
-                        openWith[mots[2]] = openWith[mots[0]];
+                    if(m1 == 0)
+                    {                       
+                        m1 = Manipulate(mots[0]);
                     }
+
+                    ManipulateFin(mots[2]);
+                    openWith[mots[2]] = (ushort)m1;
+                    
                 }
                 else if (mots[1] == "OR")
                 {
-                    byte m1 = 0;
-                    byte m2 = 0;                   
-                    byte.TryParse(mots[0], out m1);
-                    byte.TryParse(mots[2], out m2);
+                    int m1 = 0;
+                    int m2 = 0;                   
+                    int.TryParse(mots[0], out m1);
+                    int.TryParse(mots[2], out m2);
 
                     if (m1 != 0) {
-                        Manipulate(mots[2]);
-                        m2 = (byte)openWith[mots[2]];                        
+                        
+                        m2 = Manipulate(mots[2]);
                     }
 
                     else if(m2 != 0) {
-                        Manipulate(mots[0]);
-                        m1 = (byte)openWith[mots[0]];
+                        
+                        m1 = Manipulate(mots[0]);
                     }
                     else if(m1 == 0 && m2 == 0)
                     {
-                        Manipulate(mots[2]);
-                        m2 = (byte)openWith[mots[2]];
-                        Manipulate(mots[0]);
-                        m1 = (byte)openWith[mots[0]];
+                        m2 = Manipulate(mots[2]);
+                        m1 = Manipulate(mots[0]);
                     }
-
-
-                    if (mots[0] == "a" || mots[4] == "lx" || mots[4] == "lw")
+                    else
                     {
-                        bool lol;
+                        throw new Exception();
                     }
 
-                    Manipulate(mots[4]);
+                    ManipulateFin(mots[4]);
                     openWith[mots[4]] = (ushort)(m1 | m2);
                   
 
                 }
                 else if (mots[1] == "AND")
                 {
-                    byte m1 = 0;
-                    byte m2 = 0;
-                    byte.TryParse(mots[0], out m1);
-                    byte.TryParse(mots[2], out m2);
+                    int m1 = 0;
+                    int m2 = 0;
+                    int.TryParse(mots[0], out m1);
+                    int.TryParse(mots[2], out m2);
 
                     if (m1 != 0)
                     {
-                        Manipulate(mots[2]);
-                        m2 = (byte)openWith[mots[2]];
+                        
+                        m2 = Manipulate(mots[2]);
                     }
 
                     else if (m2 != 0)
                     {
-                        Manipulate(mots[0]);
-                        m1 = (byte)openWith[mots[0]];
+                        
+                        m1 = Manipulate(mots[0]);
                     }
                     else if (m1 == 0 && m2 == 0)
                     {
-                        Manipulate(mots[2]);
-                        m2 = (byte)openWith[mots[2]];
-                        Manipulate(mots[0]);
-                        m1 = (byte)openWith[mots[0]];
+                        m2 = Manipulate(mots[2]);
+                        m1 = Manipulate(mots[0]);
                     }
-
-                    if (mots[0] == "a" || mots[4] == "lv" || mots[4] == "lw")
+                    else
                     {
-                        bool lol;
+                        throw new Exception();
                     }
 
-                    Manipulate(mots[4]);
+
+                    ManipulateFin(mots[4]);
                     openWith[mots[4]] = (ushort)(m1 & m2);
 
 
                 }
                 else if (mots[1] == "RSHIFT")
                 {
-                    byte m2 = 0;
-                    byte.TryParse(mots[2], out m2);
+                    int m2 = 0;
+                    int.TryParse(mots[2], out m2);
 
-                    byte m1 = 0;
-                    Manipulate(mots[0]);
-                    m1 = (byte)openWith[mots[0]];
+                    int m1 = 0;
+                    
+                    m1 = Manipulate(mots[0]);
 
-                    Manipulate(mots[4]);
+                    ManipulateFin(mots[4]);
                     openWith[mots[4]] = (ushort)(m1 >> m2);
 
 
@@ -143,38 +122,31 @@ namespace Day7_C
 
                 else if (mots[1] == "LSHIFT")
                 {
-                    byte m2 = 0;
-                    byte.TryParse(mots[2], out m2);
+                    int m2 = 0;
+                    int.TryParse(mots[2], out m2);
 
-                    byte m1 = 0;
-                    Manipulate(mots[0]);
-                    m1 = (byte)openWith[mots[0]];
+                    int m1 = 0;
+ 
+                    m1 = Manipulate(mots[0]);
 
-
-                    Manipulate(mots[4]);
+                    ManipulateFin(mots[4]);
                     openWith[mots[4]] = (ushort)(m1 << m2);
 
-                    if (mots[0] == "a" || mots[4] == "lv")
-                    {
-                        bool lol;
-                    }
                 }
                 else
                 {
                     throw new Exception();
-                }
-
-        
+                } 
 
 
             }
 
-            Console.WriteLine(openWith["a"].ToString());
+            Console.WriteLine(openWith["a"]);
             Console.ReadLine();
 
         }
 
-        public static void Manipulate(string cle)
+        public static void ManipulateFin(string cle)
         {
 
             if (!openWith.Keys.Contains(cle))
@@ -182,6 +154,18 @@ namespace Day7_C
                 openWith.Add(cle, 0);
             }
             
+
+        }
+        public static int Manipulate(string cle)
+        {
+
+            if (!openWith.Keys.Contains(cle))
+            {
+                return 0;
+            }
+            else
+                return openWith[cle];
+
 
         }
     }
