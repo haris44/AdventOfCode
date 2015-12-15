de7d6cc794b32ce4712059260d11e14d10033807
haris44 <lxndr.bertrand@gmail.com>
2015-12-15 23:14:44 +0100

    Ajout day 12-1

---
+++ Day 12 - C/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_12___C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lines = System.IO.File.ReadAllText(@"c:\users\macpro\documents\visual studio 2015\Projects\Day 12 - C\Day 12 - C\day12.txt");
+            double total = 0;
+
+            bool moins = false;
+            string numencours = "";
+
+            bool inob = false;
+            objet encours = null;
+
+            foreach (char c in lines)
+            {
+              
+                if (c == '{' && inob == false)                  
+                {
+                    encours = new objet();
+                    inob = true;
+                }
+                else if (c == '}' && inob == true && encours.inobjet == null)
+                {
+                    encours = null;
+                    inob = false;
+                }
+                else if (inob == true)
+                {
+                   encours.checkcarac(c);
+                }
+                else if (Char.IsNumber(c))
+                {
+                    numencours += c;
+                }
+                else if (c == '-')
+                {
+                    moins = true;
+                }
+                else
+                {
+                    if (numencours != "")
+                    {
+                        if (moins)
+                            total -= (double.Parse(numencours));
+                        else
+                        {
+                            total += (double.Parse(numencours));
+                        }
+                        numencours = "";
+                        moins = false;
+                    }
+                    else
+                    {
+                        numencours = "";
+                        moins = false;
+                    }
+
+                }
+
+            }
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Day 12 - C/objet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_12___C
+{
+    class objet
+    {
+        string illegal = "red";
+        bool moins = false;
+        string numencours;
+        double total;
+        public objet inobjet == null; 
+
+    public bool verifyillegal(char now, char prec, char precprec)
+    {
+            if ("" + precprec + prec + now == illegal)
+                return false;
+            else
+                return true;
+    }
+
+    public void checkcarac(char c)
+    {
+
+        if (Char.IsNumber(c))
+        {
+            numencours += c;
+        }
+        else if (c == '-')
+        {
+            moins = true;
+        }
+        else
+        {
+            if (numencours != "")
+            {
+                if (moins)
+                    total -= (double.Parse(numencours));
+                else
+                {
+                    total += (double.Parse(numencours));
+                }
+                numencours = "";
+                moins = false;
+            }
+            else
+            {
+                numencours = "";
+                moins = false;
+            }
+
+        }
+      }
+    }
+
+
+}
